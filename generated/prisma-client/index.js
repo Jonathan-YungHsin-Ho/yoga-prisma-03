2f8435be8d721c942e214e92aa1a8da4797dee3e
Jonathan Ho <johnnyho82@gmail.com>
2019-11-16 14:31:26 -0500

    Set up gitignore file, set up package.json file, set up Prisma

---
+++ generated/prisma-client/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "User",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://practice-server-2-5097b1f8fb.herokuapp.com/yoga-prisma-03/dev`
+});
+exports.prisma = new exports.Prisma();
