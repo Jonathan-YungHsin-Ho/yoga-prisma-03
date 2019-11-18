362d366cb97eaf15e436ad3b00c279ec37992568
Jonathan Ho <johnnyho82@gmail.com>
2019-11-18 00:32:05 -0500

    Add deletePosting mutation

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -1,5 +1,6 @@
 module.exports = {
 	addPosting,
+	deletePosting,
 };
 
 function addPosting(_root, args, context) {
@@ -11,4 +12,8 @@ function addPosting(_root, args, context) {
 		price: args.price,
 		description: args.description,
 	});
+}
+
+function deletePosting(_root, args, context) {
+	return context.prisma.deletePosting({ id: args.id });
 }
--- resolvers/Query.js
+++ resolvers/Query.js
@@ -7,6 +7,6 @@ function info() {
 	return `yoga-prisma-03 is a practice API using graphql-yoga and Prisma!`;
 }
 
-async function postings(_root, _args, context) {
-	return await context.prisma.postings();
+function postings(_root, _args, context) {
+	return context.prisma.postings();
 }
