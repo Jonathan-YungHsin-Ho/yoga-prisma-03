f2ad2b6e4b27afe053eac839b05d574a5d6c82cb
Jonathan Ho <johnnyho82@gmail.com>
2019-11-18 22:38:21 -0500

    Add updatePosting mutation

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -1,19 +1,22 @@
 module.exports = {
 	addPosting,
 	deletePosting,
+	updatePosting,
 };
 
 function addPosting(_root, args, context) {
-	return context.prisma.createPosting({
-		coach: args.coach,
-		title: args.title,
-		industry: args.industry,
-		location: args.location,
-		price: args.price,
-		description: args.description,
-	});
+	return context.prisma.createPosting(args);
 }
 
 function deletePosting(_root, args, context) {
 	return context.prisma.deletePosting({ id: args.id });
+}
+
+function updatePosting(_root, args, context) {
+	const { id, coach, title, industry, location, price, description } = args;
+
+	return context.prisma.updatePosting({
+		data: { coach, title, industry, location, price, description },
+		where: { id },
+	});
 }
