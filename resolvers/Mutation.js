e0ba7028fb5a71fb75267df8603e8d35c2c21314
Jonathan Ho <johnnyho82@gmail.com>
2019-11-19 10:30:09 -0500

    Update updatePosting mutation

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -26,17 +26,19 @@ function updatePosting(_root, args, context) {
 	const { id, coach, title, industry, location, price, description } = args;
 
 	return context.prisma.updatePosting({
-		data: { coach, title, industry, location, price, description },
+		data: {
+			coach,
+			title,
+			industry: {
+				connect: { name: industry },
+			},
+			location,
+			price,
+			description,
+		},
 		where: { id },
 	});
 }
-
-// async function createManyIndustries(root, args, context) {
-// 	const industries = await args.industries.map(industry =>
-// 		context.prisma.createIndustry({ name: industry }),
-// 	);
-// 	return Promise.all(industries);
-// }
 
 function createManyIndustries(_root, args, context) {
 	return args.industries.map(industry =>
